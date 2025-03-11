7d5d9d5de3c667f4851d58333860bf850b33619e
Fernanda3245m <com.mmusicog24@alumnos.cecyteg.edu.mx>
2025-03-11 16:36:59 -0600

    Add files via upload

---
+++ practica9.py.py
@@ -0,0 +1,16 @@
+frutas=["Manzana", "pera", "Naranja", "papaya"]
+colores=["Azul", "Amaeillo", "Violeta", "Gris"]
+print("lista de frutas:", frutas)
+print("lista de colores:", colores)
+frutas.append("sandia")
+print("lista de frutas con sandia:", frutas)
+frutas.append("higos")
+frutas.append("Guanabana")
+print("lista de frutas con higo y guanabana:", frutas)
+
+frutas.remove("pera")
+print("lista de frutas con la pera eliminada:", frutas)
+
+frutas.pop(3)
+print("lista de frutas con la 3 eliminada:", frutas)
+
